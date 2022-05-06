e39df5835a58546f2945a86aa4886aa848131d1c
Kushagra733 <kushagramishra733@gmail.com>
2022-05-06 12:53:52 +0530

    make it better

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const parser = require('body-parser');
+
+
+const app = express();
+
+var blogs = [];
+app.use(parser.urlencoded({ extended: false }));
+
+
+
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+
+ 
+    res.render('home',{x:blogs});
+});
+
+app.get('/compose',(req,res)=>{
+    res.render('compose');
+});
+app.post('/compose',(req,res)=>{
+    blogs.push(req.body.tex);
+    res.redirect('/');
+});
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
+
+app.listen(3000);
